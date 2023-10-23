a5cfe5a2f16f40927ee02215da8177f2da9e6441
JordanEckford <jordan.eckford@gmail.com>
2023-10-23 14:09:09 +0100

    added functionality to search all articles

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import { UsernameContext } from "../Contexts/UsernameContext";
+import { useContext } from "react";
+
 export const NavBar = () => {
- return <nav>Home | Articles | Post Article | Account</nav>;
+ const { username } = useContext(UsernameContext);
+ return (
+  <nav>
+   <Link to="/"> Home </Link> | <Link to="/articles"> View Articles </Link> | <Link to="/postarticle"> Post Article</Link> |
+   <Link to="account"> {username === null ? "User Profile" : `${username}'s Profile`} </Link>
+  </nav>
+ );
 };
---
+++ src/Components/PostArticle.jsx
@@ -0,0 +1,3 @@
+export const PostArticle = () => {
+ return <p>Post Article Element</p>;
+};
