dd8e5aebccac957be341f0e266e9a770aad67b83
JordanEckford <jordan.eckford@gmail.com>
2023-10-27 16:25:31 +0100

    added some missing styling on the login page

--- src/Components/AccountComponents/UsernameInput.jsx
+++ src/Components/AccountComponents/UsernameInput.jsx
@@ -3,17 +3,19 @@ export const UsernameInput = ({ updateUsername, inputName, setInputName }) => {
   <div className="enter-username-container">
    Enter your username below to login:
    <form onSubmit={updateUsername}>
-    <label htmlFor="username">Username: </label>
-    <input
-     type="text"
-     id="username"
-     value={inputName}
-     onChange={(event) => {
-      setInputName(event.target.value);
-     }}
-     required
-    />
-    <button className="submit-button" type="submit">
+    <div className="username-input-container">
+     <label htmlFor="username">Username: </label>
+     <input
+      type="text"
+      id="username"
+      value={inputName}
+      onChange={(event) => {
+       setInputName(event.target.value);
+      }}
+      required
+     />
+    </div>
+    <button className="submit-button-username" type="submit">
      Submit
     </button>
    </form>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,16 +6,16 @@ export const NavBar = () => {
  const { username } = useContext(UsernameContext);
  return (
   <nav className="main-navbar">
-   <Link className="navbar-link" to="/home">
+   <Link className="navbar-link-home" to="/home">
     Home
    </Link>
-   <Link className="navbar-link" to="/articles">
+   <Link className="navbar-link-view" to="/articles">
     View Articles
    </Link>
-   <Link className="navbar-link" to="/postarticle">
+   <Link className="navbar-link-post" to="/postarticle">
     Post Article
    </Link>
-   <Link className="navbar-link" to="account">
+   <Link className="navbar-link-account" to="account">
     {username === null ? "User Profile" : `${username}'s Profile`}
    </Link>
   </nav>
