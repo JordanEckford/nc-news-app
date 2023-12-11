8e47e44a254e21fefd9dd6caa48457681ac51d0d
JordanEckford <jordan.eckford@gmail.com>
2023-12-11 17:18:12 +0000

    added message about default user

--- src/Components/HomeComponents/WelcomeCard.jsx
+++ src/Components/HomeComponents/WelcomeCard.jsx
@@ -6,6 +6,7 @@ export const WelcomeCard = () => {
   <>
    <h2 className="welcome-header">Hello, Welcome to NC News</h2>
    <p className="home-text">This is a mock news site. Feel free to browse, post, comment or like articles</p>
+   <p>By Default, you are logged in as a current user. Please logout and create an account if you would like to personalise the experience!</p>
   </>
  );
 };
